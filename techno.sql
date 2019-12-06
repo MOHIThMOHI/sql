20486f22e7089664d934bc8acc1fbdec9dc65347
MOHIThMOHI <56334759+MOHIThMOHI@users.noreply.github.com>
2019-12-06 13:42:46 +0530

    added

---
+++ techno.sql
@@ -0,0 +1,41 @@
+/*
+SQLyog Ultimate v10.00 Beta1
+MySQL - 5.1.73-community : Database - techno
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`techno` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `techno`;
+
+/*Table structure for table `t1` */
+
+DROP TABLE IF EXISTS `t1`;
+
+CREATE TABLE `t1` (
+  `name` varchar(10) DEFAULT NULL,
+  `address` varchar(20) DEFAULT NULL,
+  `usn` varchar(10) DEFAULT NULL,
+  `phone` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `t1` */
+
+LOCK TABLES `t1` WRITE;
+
+insert  into `t1`(`name`,`address`,`usn`,`phone`) values ('kal','nittur','1',11),('sarsawathi','nittur',NULL,NULL),('likith','tumkur',NULL,NULL),('(NULL)kira',NULL,NULL,NULL);
+
+UNLOCK TABLES;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
